facd6aeb9d7e1820f7999da8f69630547ec13d25
Hanshika Gupta <hanshika02@gmail.com>
2026-01-11 13:05:58 +0530

    Initial Commit

---
+++ simple-task-manager/simple_task_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_task_managerApp.swift
+//  simple-task-manager
+//
+//  Created by Hanshika Gupta on 11/01/26.
+//
+
+import SwiftUI
+
+@main
+struct simple_task_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
